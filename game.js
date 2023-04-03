7144f610602e6277ae5613d4d4eb9655623b2d76
caseybryan <caseythomasbryan@gmail.com>
2023-04-03 14:55:25 -0700

    Add files via upload

---
+++ game.js
@@ -0,0 +1,298 @@
+let gameRunning = false;
+let score = 0;
+
+const gameOverElement = document.getElementById('gameOver');
+
+
+
+const canvas = document.getElementById('gameBoard');
+const ctx = canvas.getContext('2d');
+const blockSize = 32;
+
+const tetrominoColors = [
+    'cyan',
+    'yellow',
+    'purple',
+    'green',
+    'red',
+    'blue',
+    'orange'
+];
+
+const tetrominoes = [
+    [
+        ['0110',
+         '0110']
+    ],
+    [
+        ['0010',
+         '0010',
+         '0010',
+         '0010'],
+        ['0000',
+         '1111',
+         '0000',
+         '0000']
+    ],
+    [
+        ['0100',
+         '0110',
+         '0010'],
+        ['0110',
+         '0100',
+         '0100'],
+        ['0010',
+         '0110',
+         '0100'],
+        ['0100',
+         '0100',
+         '0110']
+    ],
+    [
+        ['0010',
+         '0110',
+         '0100'],
+        ['0110',
+         '0010',
+         '0010']
+    ],
+    [
+        ['0100',
+         '0110',
+         '0010'],
+        ['0110',
+         '0100',
+         '0100']
+    ],
+    [
+        ['0100',
+         '0110',
+         '0100']
+    ]
+];
+
+
+let piece = {
+    x: 0,
+    y: 0,
+    shape: null,
+};
+
+let gameOver = false;
+
+
+function reset() {
+    const randomIndex = Math.floor(Math.random() * tetrominoes.length);
+    const randomPiece = tetrominoes[randomIndex];
+    piece.shape = [...randomPiece[Math.floor(Math.random() * randomPiece.length)]];
+    piece.color = tetrominoColors[randomIndex];
+    piece.y = 0;
+    piece.x = Math.floor(canvas.width / (2 * blockSize)) - 1;
+}
+
+
+
+const board = new Array(Math.floor(canvas.height / blockSize)).fill(null)
+    .map(() => new Array(Math.floor(canvas.width / blockSize)).fill(0));
+
+function collide() {
+    for (let y = 0; y < piece.shape.length; y++) {
+        for (let x = 0; x < piece.shape[y].length; x++) {
+            if (piece.shape[y][x] === '1') {
+                let newX = piece.x + x;
+                let newY = piece.y + y;
+
+                if (newX < 0 || newX >= canvas.width / blockSize || newY < 0 || newY >= canvas.height / blockSize) {
+                    return true;
+                }
+                if (board[newY][newX] !== 0) {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+function merge() {
+    for (let y = 0; y < piece.shape.length; y++) {
+        for (let x = 0; x < piece.shape[y].length; x++) {
+            if (piece.shape[y][x] === '1') {
+                board[piece.y + y][piece.x + x] = piece.color;
+            }
+        }
+    }
+}
+
+
+function rotate() {
+    const newShape = [];
+    for (let x = 0; x < piece.shape[0].length; x++) {
+        newShape.push([]);
+        for (let y = piece.shape.length - 1; y >= 0; y--) {
+            newShape[x].push(piece.shape[y][x]);
+        }
+    }
+    piece.shape = newShape;
+    if (collide()) {
+        piece.shape = piece.shape[0].map((_, i) => piece.shape.map(row => row[i])).reverse();
+    }
+}
+
+function move(dir) {
+    piece.x += dir;
+    if (collide()) {
+        piece.x -= dir;
+    }
+}
+
+function clearRows() {
+    let linesCleared = 0;
+    outer: for (let y = board.length - 1; y >= 0; y--) {
+        for (let x = 0; x < board[y].length; x++) {
+            if (board[y][x] === 0) {
+                continue outer;
+            }
+        }
+
+        const row = board.splice(y, 1)[0].fill(0);
+        board.unshift(row);
+        y++;
+        linesCleared++;
+    }
+
+    if (linesCleared > 0) {
+        const lineScores = [40, 100, 300, 1200];
+        score += lineScores[linesCleared - 1];
+    }
+}
+
+let scoreElement = document.getElementById('score');
+
+function updateScore(newScore) {
+    score = newScore;
+    scoreElement.innerText = "Score: " + score;
+}
+
+
+function drop() {
+    piece.y++;
+    if (collide()) {
+        piece.y--;
+        merge();
+        clearRows();
+        reset();
+        if (collide()) {
+            gameOver = true;
+            board.forEach(row => row.fill(0));
+        }
+    }
+    dropCounter = 0;
+}
+
+function draw() {
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // Draw the static blocks on the board
+    for (let y = 0; y < board.length; y++) {
+        for (let x = 0; x < board[y].length; x++) {
+            if (board[y][x] !== 0) {
+                ctx.fillStyle = board[y][x];
+                ctx.fillRect(x * blockSize, y * blockSize, blockSize, blockSize);
+                ctx.strokeStyle = 'black';
+                ctx.strokeRect(x * blockSize, y * blockSize, blockSize, blockSize);
+            }
+        }
+    }
+
+    // Draw the current tetromino
+    ctx.fillStyle = piece.color;
+    for (let y = 0; y < piece.shape.length; y++) {
+        for (let x = 0; x < piece.shape[y].length; x++) {
+            if (piece.shape[y][x] !== '0') {
+                ctx.fillRect((piece.x + x) * blockSize, (piece.y + y) * blockSize, blockSize, blockSize);
+                ctx.strokeStyle = 'black';
+                ctx.strokeRect((piece.x + x) * blockSize, (piece.y + y) * blockSize, blockSize, blockSize);
+            }
+        }
+    }
+
+    // Update and display the score
+    scoreElement.innerHTML = `Score: ${score}`;
+}
+
+
+function gameLoop() {
+    draw();
+    drop();
+    requestAnimationFrame(gameLoop);
+}
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'ArrowUp') {
+        rotate();
+    } else if (e.key === 'ArrowRight') {
+        move(1);
+    } else if (e.key === 'ArrowLeft') {
+        move(-1);
+    } else if (e.key === 'ArrowDown') {
+        drop();
+    }
+});
+
+function animateGameOver() {
+    for (let y = 0; y < board.length; y++) {
+        for (let x = 0; x < board[y].length; x++) {
+            if (board[y][x] !== 0) {
+                board[y][x] = 0;
+                board[Math.min(y + 1, board.length - 1)][x] = 0;
+            }
+        }
+    }
+}
+
+function startGame() {
+    if (gameRunning) return;
+
+    gameOverElement.style.display = 'none';
+    gameRunning = true;
+    gameOver = false;
+    board.forEach(row => row.fill(0));
+    score = 0;
+    updateScore(score);
+    reset();
+    update();
+}
+
+
+
+let lastTime = 0;
+let dropCounter = 0;
+const dropInterval = 500; // In milliseconds
+
+function update(time = 0) {
+        if (gameOver) {
+        gameRunning = false;
+        gameOverElement.style.display = 'block';
+        return;
+    }
+
+    const deltaTime = time - lastTime;
+    lastTime = time;
+
+    dropCounter += deltaTime;
+    if (dropCounter > dropInterval) {
+        drop();
+        dropCounter = 0;
+    }
+
+    draw();
+    requestAnimationFrame(update);
+}
+
+   
+document.getElementById('startButton').addEventListener('click', startGame);
+
+reset();
+update();
